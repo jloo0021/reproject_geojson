f36c60c1f85279ce34ae1e5557caa32e0bde8bee
Joshua Loong <jloo0021@student.monash.edu>
2022-12-08 20:57:34 +1100

    Wrote buffer function. TODO: Test the output file in mapbox app, see if it loads quicker. If so, consider combining reproject and buffer functions together

--- reproject_geojson_coords.py
+++ reproject_geojson_coords.py
@@ -34,20 +34,48 @@ def reproject_geojson_coords(source_epsg_code: int, dest_epsg_code: int, geojson
         outfile.write(json.dumps(geojson))
 
 
+# converts all features in a geojson from multiline strings to polygons by using shapely buffer
+def buffer_multilinestr_to_polygon(geojson, buffer_size: int):
+    # iterate over features
+    num_features = len(geojson["features"])
+    for i in range(num_features):
+        feature = geojson["features"][i]
+
+        # each feature should be a multiline string, so we want to create a shapely multiline string using its coords
+        multiline_str = multilinestring.MultiLineString(feature["geometry"]["coordinates"])
+
+        # apply buffer to turn each feature into a polygon and map it into geojson geometry form
+        buffered_feature = mapping(multiline_str.buffer(buffer_size))
+
+        # overwrite existing multiline string feature geometry with new polygon feature geometry
+        geojson["features"][i]["geometry"] = buffered_feature
+
+    # write transformed geojson to a new file
+    with open("buffered_geojson.geojson", "w") as outfile:
+        outfile.write(json.dumps(geojson))
+
+
 if __name__ == "__main__":
 
+    # # EDIT THESE VARIABLES TO MAKE THE SCRIPT DO WHAT YOU WANT
+    # file_path = r"D:\Software Projects\Multiviz Internship\Misc GeoJSON Files\liveability_sa1_2011_difference.geojson"
+    # source_epsg_code = 7845
+    # dest_epsg_code = 4236
+    #
+    # # load geojson from file
+    # with open(file_path, "r") as infile:
+    #     geojson = json.load(infile)
+    #
+    # # reproject from ... to ..., writes to a new geojson file with the new projection
+    # reproject_geojson_coords(source_epsg_code, dest_epsg_code, geojson)
+
     # EDIT THESE VARIABLES TO MAKE THE SCRIPT DO WHAT YOU WANT
-    file_path = r"D:\Software Projects\Multiviz Internship\Misc GeoJSON Files\liveability_sa1_2011_difference.geojson"
-    source_epsg_code = 7845
-    dest_epsg_code = 4236
+    file_path = r"D:\Software Projects\Multiviz Internship\Experimenting Repo\wombling_experiment\dummy.geojson"
 
     # load geojson from file
     with open(file_path, "r") as infile:
         geojson = json.load(infile)
 
-    # reproject from ... to ..., writes to a new geojson file with the new projection
-    reproject_geojson_coords(source_epsg_code, dest_epsg_code, geojson)
-
-
+    buffer_multilinestr_to_polygon(geojson, 1)
 
 
